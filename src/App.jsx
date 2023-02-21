27a87399ebe3ba08c8d03130e012d8c84730cc6d
Yunus Ozbay <yunus.ozbay@hotmail.com>
2023-02-21 18:02:12 +0100

    it 5 done

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,66 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import "./App.css";
+import { useState } from "react";
+import contacts from "./contacts.json";
+
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [firstFive, setFirstFive] = useState(contacts.slice(0, 5));
+  const [remaining, setRemaining] = useState(contacts.slice(5))
+  function randomContact() {
+    const random = Math.round(Math.random() * remaining.length)
+    const contact = remaining.splice(random, 1)[0]
+    console.log(remaining.length) 
+    setFirstFive([...firstFive, contact])
+  }
+ /*  function delete() {
+    const id = 
+  } */
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Ironcontacts</h1>
+      <button onClick={() => randomContact()}>
+          Add Random Contact
+      </button>
+      <button onClick={() => setFirstFive((firstFive) => [...firstFive].sort((a, b) => (a.name > b.name) ? 1 : -1))}>
+          Sort By Name
+      </button>
+      <button onClick={() => setFirstFive((firstFive) => [...firstFive].sort((a, b) => (a.popularity < b.popularity) ? 1 : -1))}>
+          Sort By Popularity
+      </button>
+      <table id="table">
+        <thead>
+          <tr> 
+            <th>Picture</th>
+            <th>Name</th>
+            <th>Popularity</th>
+            <th>Won<br></br>Oscar</th>
+            <th>Won<br></br>Emmy</th>
+            <th>Actions</th>
+         </tr>
+        </thead>
+        <tbody>
+        {firstFive.map((x, index) => (
+          <tr key={x.id}>
+            <td>
+              <img src={x.pictureUrl} alt="pic" />
+            </td>
+            <td>{x.name}</td>
+            <td>{Math.round(x.popularity * 100) / 100}</td>
+            <td>{x.wonOscar === true ? '🏆' : ''}</td>
+            <td>{x.wonEmmy === true ? '🏆' : ''}</td>
+            <td>
+              <button onClick={() => setFirstFive((firstFive) => firstFive.splice(index, 1) )}>
+                Delete
+              </button>
+            </td>
+          </tr>
+        ))}
+        </tbody>
+      </table>
     </div>
-  )
+  );
 }
+export default App;
 
-export default App
+
